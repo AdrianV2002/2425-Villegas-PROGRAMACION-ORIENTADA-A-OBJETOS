fe82debbb523b93a81d69dc843ba1516306edbc2
DqnyLux <i_dqni@outlook.com>
2024-12-14 17:06:49 -0500

    Tarea Semana#3 Programacion POO.

--- Bloque #1/Semana#3/Semana3.Programación Tradicional y POO.py
+++ Bloque #1/Semana#3/Semana3.Programación Tradicional y POO.py
@@ -17,3 +17,31 @@ def programa_tradicional():
     print(f"El promedio semanal de las temperaturas es: {promedio:.2f} grados.")
 
 programa_tradicional()
+
+
+
+
+
+# Programación Orientada a Objetos (POO)
+
+class ClimaSemanal:
+    def __init__(self):
+        self.temperaturas = []
+
+    def ingresar_temperaturas(self):
+        for dia in range(7):
+            temp = float(input(f"Ingresa la temperatura del día {dia + 1}: "))
+            self.temperaturas.append(temp)
+
+    def calcular_promedio(self):
+        return sum(self.temperaturas) / len(self.temperaturas)
+
+# Ejecución del programa orientado a objetos
+def programa_poo():
+    print("Programación Orientada a Objetos: Calculo del Promedio Semanal del Clima")
+    clima = ClimaSemanal()
+    clima.ingresar_temperaturas()
+    promedio = clima.calcular_promedio()
+    print(f"El promedio semanal de las temperaturas es: {promedio:.2f} grados.")
+
+programa_poo()
